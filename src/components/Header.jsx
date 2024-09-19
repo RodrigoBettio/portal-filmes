50ef391d8173ce8e41e8192e1df5e45eda7b9ee8
Rodrigo Bettio <rodriguinhobettiojr1@gmail.com>
2024-09-18 22:51:48 -0300

    Colocando as imagens e ids no saiba mais

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,12 @@ import { NavLink } from "react-router-dom";
 export default function Header() {
     return (
         <>
-            <header className="flex bg-green-100 text-blue-700"> 
-                <div>
+            <header className="flex bg-blue-500 text-white justify-around items-center h-14"> 
+                <div className="font-extrabold">
                     <h1>Portal Filmes</h1>
                 </div>
                 <nav>
-                    <ul>
+                    <ul className="flex gap-6">
                         <li><NavLink to="/">Home</NavLink></li>
                         <li><NavLink to="/movies">Filmes</NavLink></li>
                         <li><NavLink to="/genre">Gêneros</NavLink></li>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,18 @@
+import CardContainer from "../components/CardContainer";
+import MovieCard from "../components/MovieCard"
+import movies from "../data/movies.json"
+
+
 export default function Home(){
     return(
-        <h1> Home Page</h1>
+        <>
+            <CardContainer titulo="Filmes antigos">
+            {
+                movies.map(filme => (
+                    <MovieCard key={filme.id}{...filme}/> //Spread Array
+                ))
+            }
+            </CardContainer>
+        </>
     )
 }
